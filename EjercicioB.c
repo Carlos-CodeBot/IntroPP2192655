d93125c6e546b452f5c20f2e09a198646fae45fd
Carlos Josseph Rodriguez Hernandez <83562533+Carlos-CodeBot@users.noreply.github.com>
2023-09-29 11:43:35 -0500

    1.3

--- EjercicioB.c
+++ EjercicioB.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>  // Necesario para usar OpenMP.
 
-// Definición de las dimensiones de las matrices.
-// Esto facilita el cambio de dimensiones en un solo lugar.
-#define ROWS_M1 3
-#define COLS_M1 2
-#define ROWS_M2 2
-#define COLS_M2 3
+#define ROWS_M1 1000
+#define COLS_M1 1000
+#define ROWS_M2 1000
+#define COLS_M2 1000
 
-/*
- * Función para multiplicar dos matrices.
- * Las matrices son pasadas como argumentos junto con una matriz resultante.
- * La matriz resultante se modificará con el resultado de la multiplicación.
- */
-void multiplyMatrix(int m1[ROWS_M1][COLS_M1], int m2[ROWS_M2][COLS_M2], int result[ROWS_M1][COLS_M2]) {
-    // Verificar que el número de columnas de m1 es igual al número de filas de m2.
+// Función para multiplicar dos matrices.
+void multiplyMatrix(int **m1, int **m2, int **result) {
+    // Verificar que se puedan multiplicar las matrices.
     if (COLS_M1 != ROWS_M2) {
         fprintf(stderr, "Error: El número de columnas de la primera matriz debe ser igual al número de filas de la segunda matriz.\n");
         exit(EXIT_FAILURE);
     }
 
-    // Multiplicación de matrices.
+    // Paralelización de la multiplicación de matrices usando OpenMP.
+    #pragma omp parallel for
     for (int i = 0; i < ROWS_M1; i++) {
         for (int j = 0; j < COLS_M2; j++) {
-            result[i][j] = 0;  // Inicializar el elemento resultante.
+            result[i][j] = 0;
             
-            // Calcular el valor de result[i][j] como el producto punto de la fila i de m1 y la columna j de m2.
             for (int k = 0; k < COLS_M1; k++) {
                 result[i][j] += m1[i][k] * m2[k][j];
             }
@@ -34,32 +29,54 @@ void multiplyMatrix(int m1[ROWS_M1][COLS_M1], int m2[ROWS_M2][COLS_M2], int resu
 }
 
 int main() {
-    // Declarar e inicializar las matrices m1 y m2.
-    int m1[ROWS_M1][COLS_M1] = {
-        {1, 1},
-        {2, 2},
-        {3, 3}
-    };
+    // Asignación dinámica de memoria para las matrices.
+    int **m1 = (int **)malloc(ROWS_M1 * sizeof(int *));
+    int **m2 = (int **)malloc(ROWS_M2 * sizeof(int *));
+    int **result = (int **)malloc(ROWS_M1 * sizeof(int *));
 
-    int m2[ROWS_M2][COLS_M2] = {
-        {1, 1, 1},
-        {2, 2, 2}
-    };
+    // Inicializar la matriz m1 con todos los elementos en 1.
+    for (int i = 0; i < ROWS_M1; i++) {
+        m1[i] = (int *)malloc(COLS_M1 * sizeof(int));
+        result[i] = (int *)malloc(COLS_M2 * sizeof(int));
+        
+        for (int j = 0; j < COLS_M1; j++) {
+            m1[i][j] = 1;
+        }
+    }
 
-    // Declarar una matriz para almacenar el resultado.
-    int result[ROWS_M1][COLS_M2];
-    
-    // Llamar a la función de multiplicación de matrices.
+    // Inicializar la matriz m2 con todos los elementos en 2.
+    for (int i = 0; i < ROWS_M2; i++) {
+        m2[i] = (int *)malloc(COLS_M2 * sizeof(int));
+        
+        for (int j = 0; j < COLS_M2; j++) {
+            m2[i][j] = 2;
+        }
+    }
+
+    // Llamada a la función de multiplicación.
     multiplyMatrix(m1, m2, result);
 
-    // Imprimir la matriz resultante.
+    // (Opcional) Imprimir el resultado. Puede ser muy extenso, así que está comentado.
+    // for (int i = 0; i < ROWS_M1; i++) {
+    //     for (int j = 0; j < COLS_M2; j++) {
+    //         printf("%d\t", result[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // Liberar la memoria asignada dinámicamente.
     for (int i = 0; i < ROWS_M1; i++) {
-        for (int j = 0; j < COLS_M2; j++) {
-            printf("%d\t", result[i][j]);
-        }
-        printf("\n");
+        free(m1[i]);
+        free(result[i]);
     }
+
+    for (int i = 0; i < ROWS_M2; i++) {
+        free(m2[i]);
+    }
+
+    free(m1);
+    free(m2);
+    free(result);
 
     return 0;
 }
-
